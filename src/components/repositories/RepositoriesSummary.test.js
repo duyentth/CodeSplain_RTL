7d3c67ccabb0b7e0362869df5e05e35b9cc2ee5a
duyentth <duyen.tran@miu.edu>
2023-10-20 17:02:03 -0400

    updated the testcase displaying info of repository

--- src/components/repositories/RepositoriesSummary.test.js
+++ src/components/repositories/RepositoriesSummary.test.js
@@ -1,10 +1,17 @@
-import {screen, render} from "@testing-library/react";
+import { screen, render } from "@testing-library/react";
 import RepositoriesSummary from "./RepositoriesSummary";
 
-test("primary language should be shown on screen", () => {
-    const repository = { stargazers_count: 20, 
-        open_issues: 2, forks: 100 , language: "Javascript"};
-    render(<RepositoriesSummary repository ={repository}/>);
-    const language = screen.getByText(/Javascrip/i);
-    expect(language).toBeInTheDocument();
+test("display information of repository", () => {
+    const repository = {
+        stargazers_count: 20,
+        open_issues: 12,
+        forks: 100,
+        language: "Javascript",
+    };
+    render(<RepositoriesSummary repository={repository} />);
+    for (let key in repository) {
+        const value = repository[key];
+        const element = screen.getByText(new RegExp(value));
+        expect(element).toBeInTheDocument();
+    }
 });
